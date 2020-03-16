5a83a94c3be4584fa725bd47ee714d6652ad7806
kbh3307 <61855314+kbh3307@users.noreply.github.com>
2020-03-16 13:03:51 +0900

    주석만 달아봄

--- app/src/main/java/kr/co/tjoeun/helloworld/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/helloworld/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+//    첫 프로젝트 주석
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
